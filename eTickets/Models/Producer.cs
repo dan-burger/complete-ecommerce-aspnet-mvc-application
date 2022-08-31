5456cab75192d7bafeeaaf7f13e8860120366959
dan-burger <burger.dan4@gmail.com>
2022-08-31 16:42:26 -0400

    created models, initalized database

---
+++ eTickets/Models/Producer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+
+        //relationships
+
+        public List<Movie> Movies { get; set; }
+    }
+}
--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,7 +1,13 @@
+using eTickets.Data;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnectionString"]));
 
 var app = builder.Build();
 
@@ -13,6 +19,8 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
@@ -23,5 +31,8 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//Seed Database
+AppDbInitializer.Seed(app);
 
 app.Run();
